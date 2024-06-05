d84c86d6ccdb396209545505480ca1b339088055
Riteshyadav0101 <162138192+Riteshyadav0101@users.noreply.github.com>
2024-06-05 14:35:10 +0530

    Add files via upload

---
+++ forloop.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    // int n ;
+    // cout<<"enter the value of n"<<endl;
+    // cin>>n;
+
+    // cout<<"printing count from 1 to n"<<endl;
+
+    // for(int i = 1; i<=n; i++){
+    //     cout<<i<<endl;
+    // }
+    int name;
+    int age;
+    cin>>name;
+    cin>>age;
+    cout<<"The name of the person is"<<name<<"and the age is"<<age<<end
+    
+
+
+}
